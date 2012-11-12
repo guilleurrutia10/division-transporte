2ebfd4b9291282a5e64e3c3278cacea1f69baf37
LeoMorales <leo.moralesr23@gmail.com>
2012-11-12 18:00:11 -0300

    Nueva clase de negocio Division de Transporte

--- src/negocio/Division_Transporte.py
+++ src/negocio/Division_Transporte.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 '''
 Created on 28/10/2012
 
 @author: urrutia
 '''
+from persistent import Persistent
 
 from Empleado import *
 from Legajo import *
@@ -13,33 +15,79 @@ from Seccion import *
 from TipoDeReparacion import *
 from TipoDocumento import *
 
-class Division_Transpoerte(object):
-    
+class Division_Transporte(Persistent):
     """
+    Clase que representa a la División de Transporte.
     :version:
     :author:
     """
     
-    def __init__(self):
-        '''
-        @return: 
-        @author: 
-        '''
-        pass
+    """ ATTRIBUTES
+        
+        legajos
+        
+        localidades
+        
+        empleados
+        
+        tiposDocumentos
+        
+        repuestos
+        
+        tiposDeReparacion
+        
+        secciones
+        
+    """
     
-    def getInstancia(self):
+    instance = None
+    
+    def __new__(cls, *args, **kargs):
+        '''
+        @return: Division_Transporte
+        @author: 
+        '''
+        if cls.instance is None:
+            cls.instance = Persistent.__new__(cls, *args, **kargs)
+            cls.instance.inicialize()
+        return cls.instance
+    
+    def inicialize(self):
         '''
         @return: 
         @author: 
         '''
-        pass
+        self.instance.id = 1
+        self.instance.secciones = {}
+        self.instance.ordenesDeReparacion = {}
+        self.instance.tiposDeDocumentos = {}
+        self.instance.vehiculos = {}
+        self.instance.tiposDeDocumentos = {}
+    
+    @classmethod
+    def getInstancia(cls, *args, **kargs):
+        '''
+        @return: 
+        @author: 
+        '''
+        if cls.instance is None:
+            cls.instance = Persistent.__new__(cls, *args, **kargs)
+            cls.instance.inicialize()
+        return cls.instance
     
     def getSecciones(self):
         '''
         @return: 
         @author: 
         '''
-        pass
+        return self.secciones
+        
+    def getTipoDeDocumentos(self):
+        '''
+        @return: 
+        @author: 
+        '''
+        return self.tiposDeDocumentos
     
     def getOrdenesDeReparacion(self):
         '''
@@ -102,14 +150,14 @@ class Division_Transpoerte(object):
         @return: 
         @author: 
         '''
-        pass
+        return self.vehiculos
     
-    def cargarVehiculo(self):
+    def addVehiculo(self, dominio, marca, registroInterno, numeroChasis):
         '''
         @return: 
         @author: 
         '''
-        pass
+        self.vehiculos[dominio] = Legajo(dominio, marca, registroInterno, numeroChasis)
     
     def registrarEgresoDeVehiculo(self):
         '''
